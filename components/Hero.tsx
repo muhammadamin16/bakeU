28f6994b2f8238e352198d97eb11748610c6ddb6
shukurillaevumar <makeman707@gmail.com>
2025-12-04 13:35:15 +0500

    chanes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,7 @@
 export default function Hero() {
   return (
     <div className="bg-white rounded-xl p-4 shadow-md">
-      <h1 className="uppercase">bake the cookies</h1>
+      <h1 className="uppercase text-4xl">bake the cookies</h1>
     </div>
   );
 }
