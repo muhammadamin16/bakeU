828ead449ec30275e945f554a397531a457d4e3f
shukurillaevumar <makeman707@gmail.com>
2025-12-11 14:11:26 +0500

    Header and hero is done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,44 @@
+import { ChevronRight } from "lucide-react";
+
 export default function Hero() {
   return (
-    <div className="bg-white rounded-xl p-4 shadow-md">
-      <h1 className="uppercase text-4xl">bake the cookies</h1>
+    <div className="bg-white rounded-[32px] p-10 shadow-md">
+      <h1 className="uppercase text-[64px] md:text-[90px] leading-[0.8] font-black origin-left [transform:scaleX(1.1)] tracking-[0.05em]">
+        bake the cookies
+      </h1>
+
+      <div className="mt-6 flex items-start justify-between gap-8">
+        <div className="flex flex-col gap-4">
+          <p className="uppercase text-3xl md:text-4xl font-semibold">
+            Premium bread <br /> and cookies made <br /> from scratch
+          </p>
+          <span className="inline-flex items-center px-4 py-1 rounded-full bg-sky-500 text-white font-semibold w-fit">
+            Fresh
+          </span>
+          <p className="text-lg md:text-xl">
+            We're literally obsessed with <br /> giving more of what you love!
+          </p>
+          <div className="flex items-center gap-4 text-xl">
+            <button className="bg-amber-300 rounded-full px-6 py-3 cursor-pointer border-2 border-black font-semibold">
+              Order Now
+            </button>
+            <button className="flex items-center gap-1">
+              Cooking blog
+              <ChevronRight />
+            </button>
+          </div>
+        </div>
+
+        <div className="w-[600px] h-[280px] rounded-[32px] overflow-hidden bg-yellow-300 flex items-end justify-center">
+          <img
+            width={600}
+            height={300}
+            src="/hero_photo.jpg"
+            className="h-full object-cover"
+            alt="hero photo"
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,8 +1,24 @@
+import { ShoppingBag } from "lucide-react";
+
 export default function Header() {
   return (
-    <div>
-      <h1>BakeU</h1>
-      <p></p>
+    <div className="container mx-auto flex justify-between items-center p-2">
+      <h1 className="text-4xl">BakeU</h1>
+      <ul className="flex gap-10 text-xl">
+        <li>Cake</li>
+        <li>Bakery</li>
+        <li>About us</li>
+        <li>Cookies</li>
+        <li>Contact</li>
+      </ul>
+      <div className="flex gap-4 items-center">
+        <button className="text-xl bg-amber-300 rounded-full px-4 py-2 cursor-pointer border-2 border-black">
+          Start baking
+        </button>
+        <div className="flex items-center justify-center p-3 bg-white rounded-full cursor-pointer">
+          <ShoppingBag />
+        </div>
+      </div>
     </div>
   );
 }
