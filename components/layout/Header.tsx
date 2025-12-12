8f8667f27aadd1dcdfca2706bdbdf23ed8d6a2af
Muhammadumar <makeman707@gmail.com>
2025-12-12 21:31:48 +0500

    created carousel

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,18 +1,52 @@
-import { GalleryVerticalEnd, Heart, ShoppingBag, User, UserRoundSearch } from "lucide-react";
+import {
+  GalleryVerticalEnd,
+  Heart,
+  ShoppingBag,
+  User,
+  UserRoundSearch,
+} from "lucide-react";
 import Link from "next/link";
+
 export default function Header() {
   return (
     <div className="container mx-auto flex justify-between items-center m-3 ">
-      <h1 className="text-4xl"><Link href="/">BakeU</Link></h1>
-      <ul className="flex gap-10 text-xl">
-        <li><Link href="/catalog" className="flex"><GalleryVerticalEnd />Каталог</Link></li>
-        <li><Link href="/sellers" className="flex"><UserRoundSearch/>Продовцы</Link></li>
-        <li className=" w-md bg-white border rounded-xl overflow-hidden flex"><input type="text" placeholder="Искать товары и категории" className="flex-1 px-4 py-2.5 text-sm outline-none"/></li>
+      <h1 className="text-4xl">
+        <Link href="/">BakeU</Link>
+      </h1>
+      <ul className="flex gap-10 text-xl items-center">
+        <li>
+          <Link href="/catalog" className="flex">
+            <GalleryVerticalEnd />
+            Каталог
+          </Link>
+        </li>
+        <li>
+          <Link href="/sellers" className="flex">
+            <UserRoundSearch />
+            Продавцы
+          </Link>
+        </li>
+        <li className=" w-md bg-white border rounded-xl overflow-hidden flex">
+          <input
+            type="text"
+            placeholder="Искать товары и категории"
+            className="flex-1 px-4 py-2.5 text-sm outline-none"
+          />
+        </li>
       </ul>
       <div className="flex">
-        <Link href="/accaunt" className="flex cursor-pointer p-3"><User/>Войти</Link>
-        <Link href="/favorites" className="flex cursor-pointer p-3"><Heart />Избранное</Link>
-        <Link href="/store" className="flex cursor-pointer p-3"><ShoppingBag />Корзина</Link>
+        <Link href="/accaunt" className="flex cursor-pointer p-3">
+          <User />
+          Войти
+        </Link>
+        <Link href="/favorites" className="flex cursor-pointer p-3">
+          <Heart />
+          Избранное
+        </Link>
+        <Link href="/store" className="flex cursor-pointer p-3">
+          <ShoppingBag />
+          Корзина
+        </Link>
       </div>
     </div>
   );
